da48138a60bb5ba4f3edd2a73404588e33f29362
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2017-03-27 11:17:28 +0200

    building package archi

---
+++ src/com/polytech/smtp/client/test.java
@@ -0,0 +1,7 @@
+package com.polytech.smtp.client;
+
+/**
+ * Created by kifkif on 27/03/2017.
+ */
+public class test {
+}
---
+++ src/com/polytech/smtp/server/test.java
@@ -0,0 +1,7 @@
+package com.polytech.smtp.server;
+
+/**
+ * Created by kifkif on 27/03/2017.
+ */
+public class test {
+}

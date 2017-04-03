c841c2359bdf1fdff08602fceed178558f29de6e
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2017-04-03 09:47:50 +0200

    add noop method

--- src/com/polytech/smtp/server/factory/SMTPCommunicationFactory.java
+++ src/com/polytech/smtp/server/factory/SMTPCommunicationFactory.java
@@ -3,6 +3,7 @@ package com.polytech.smtp.server.factory;
 import com.polytech.smtp.server.SMTPCommunication;
 import com.polytech.smtp.server.method.ConnectionMethod;
 import com.polytech.smtp.server.method.Method;
+import com.polytech.smtp.server.method.NoopMethod;
 import com.polytech.smtp.server.status.Status;
 
 /**
@@ -14,16 +15,26 @@ public class SMTPCommunicationFactory implements CommunicationFactory {
     public void buildCommunication(SMTPCommunication communication)
     {
         Method connection = new ConnectionMethod();
+        Method noop = new NoopMethod();
 //        Method rset = new RESTMethod();
 
         Status authorization = new Status("authorization");
         authorization.addMethod(connection);
+        authorization.addMethod(noop);
 
         Status waitingMail = new Status("waiting_mail");
-        authorization.addMethod(connection);
+        authorization.addMethod(noop);
+
+        Status waitingData = new Status("waiting_data");
+        authorization.addMethod(noop);
+
+        Status receive = new Status("receive");
+        authorization.addMethod(noop);
 
         communication.addStatus(authorization);
         communication.addStatus(waitingMail);
+        communication.addStatus(waitingData);
+        communication.addStatus(receive);
 
         communication.setStatus("authorization");
     }

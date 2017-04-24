55e76ec523944fc3289149e06918943929bade2a
corentinmarechal <corentin.marechal@gmail.com>
2017-04-24 09:21:07 +0200

    Rset Method

--- src/com/polytech/smtp/server/method/RsetMethod.java
+++ src/com/polytech/smtp/server/method/RsetMethod.java
@@ -1,5 +1,7 @@
 package com.polytech.smtp.server.method;
 
+import com.polytech.smtp.server.mail.Mailer;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -16,6 +18,7 @@ public class RsetMethod extends SMTPMethod {
     public boolean processCommand(List<String> lines) {
         try {
             communication.setStatus("waiting_mail");
+            Mailer.getInstance().reset();
             send250("OK");
         } catch (IOException ioe) {
             ioe.printStackTrace();
